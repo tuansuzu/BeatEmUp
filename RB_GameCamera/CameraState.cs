a087d0cf9b329df06d75b59f9c9b8e7c1c338564
tuansuzu <mr.buiminhtuan@yahoo.com>
2020-03-12 15:44:01 +0700

    Add files via upload

---
+++ RB_GameCamera/CameraState.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public class CameraState : StateMachineBehaviour
+    {
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            CameraTrigger[] arr = System.Enum.GetValues(typeof(CameraTrigger)) as CameraTrigger[];
+
+            foreach (CameraTrigger t in arr)
+            {
+                CameraManager.Instance.CAM_CONTROLLER.ANIMATOR.
+                    ResetTrigger(HashManager.Instance.DicCameraTriggers[t]);
+            }
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.normalizedTime > 0.7f)
+            {
+                if (stateInfo.IsName("Shake"))
+                {
+                    animator.SetTrigger(HashManager.Instance.DicCameraTriggers[CameraTrigger.Default]);
+                }
+            }
+        }
+    }
+}
